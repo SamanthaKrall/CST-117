889749f32fe34bbb8dc6bc12b1815760f593a811
Samantha Krall <36462977+SamanthaKrall@users.noreply.github.com>
2018-05-21 18:20:51 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace KrallSamantha_Exercise1
+{
+    partial class Exercise1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label.Location = new System.Drawing.Point(45, 28);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(77, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "This is a label! ";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exitButton.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.exitButton.Location = new System.Drawing.Point(13, 61);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(151, 81);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Exercise1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 210);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label);
+            this.Name = "Exercise1Form";
+            this.Text = "Exercise1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrallSamantha_Exercise1
+{
+    public partial class Exercise1Form : Form
+    {
+        public Exercise1Form()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
