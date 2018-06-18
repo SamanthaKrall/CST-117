67eba7533575e66bba3ae7413b433e18dab900ee
Lydia <Lydia.Fritz@gcu.edu>
2018-06-18 10:13:07 -0700

    Update Form1.cs
    Let me know if this makes sense.

--- KrallSamantha_Exercise6/KrallSamantha_Exercise6/Form1.cs
+++ KrallSamantha_Exercise6/KrallSamantha_Exercise6/Form1.cs
@@ -19,11 +19,18 @@ using System.Windows.Forms;
 
 namespace KrallSamantha_Exercise6
 {
+    //see my comments in the rollButton_Click method about this variable
+    public static int counter = 0;
+    
+    
     public partial class SnakeEyes : Form
     {
         public SnakeEyes()
         {
             InitializeComponent();
+            //create 2 6-sided die
+            Dice d1 = new Dice(6);
+            Dice d2 = new Dice(6);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -36,10 +43,22 @@ namespace KrallSamantha_Exercise6
             this.Close();
         }
 
+        //in this method, you can simply roll each die and then update the text
+        //you do need a counter. Declare a static int in this file and increment
+        //every button click.  Also declare your two die objects here (see above).
         private void rollButton_Click(object sender, EventArgs e)
-        {            
-            while (Dice.sum != 1)
+        {     
+            //increment the counter
+            counter++;
+            //roll each die
+            int roll1 = d1.RollDie();
+            int roll2 = d2.RollDie();
+            
+            //change this test
+            //while (Dice.sum != 1)
+            while(roll1 != 1 || roll2 != 1)
             {
+            //change this as needed
                 die1TB.Text += Convert.ToString(Dice.result1) + "\r\n";
                 die2TB.Text += Convert.ToString(Dice.result2) + "\r\n";
                 rollsTB.Text = Convert.ToString(Dice.rolls);
