1ccc58909396d74b5e62a93e5a6541bee0929846
Samantha Krall <36462977+SamanthaKrall@users.noreply.github.com>
2018-06-27 19:55:58 -0400

    Add files via upload

---
+++ KrallSamantha_Exercise10/KrallSamantha_Exercise10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// Author: Samantha Krall
+// CST-117
+// June 2018
+
+namespace KrallSamantha_Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader testFile = new StreamReader("testFile.txt");
+                string[] input = testFile.ReadToEnd().Split();
+                int count = 0;
+                
+                foreach (string subString in input)
+                {
+                    if (char.IsPunctuation(subString, subString.Length - 1))
+                    {
+                        string modifiedString = subString.Remove(subString.Length - 1);
+
+                        while (char.IsPunctuation(modifiedString, modifiedString.Length - 1))
+                        {
+                            modifiedString = modifiedString.Remove(modifiedString.Length - 1);
+                        }
+                        
+                        if (modifiedString.EndsWith("t") || modifiedString.EndsWith("e") ||
+                            modifiedString.EndsWith("T") || modifiedString.EndsWith("E"))
+                        {
+                            count++;
+                        }
+                    }
+                    else 
+                    {
+                        
+                        if (subString.EndsWith("t") || subString.EndsWith("e") ||
+                            subString.EndsWith("T") || subString.EndsWith("E"))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("There are " + count + " words that end in 't' or 'e'");
+                testFile.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
