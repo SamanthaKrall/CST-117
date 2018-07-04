5d7a805a7ea0c1211f0bde19951f2d1b8cce614a
Samantha Krall <36462977+SamanthaKrall@users.noreply.github.com>
2018-07-03 20:37:36 -0400

    Add files via upload

---
+++ KrallSamantha_MS2/KrallSamantha_MS2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Author: Samantha Krall
+// CST-117
+
+namespace KrallSamantha_MS2
+{
+    class InventoryManager
+
+    {
+        static void Main(string[] args)
+        {
+            InventoryItem item = new InventoryItem("Cherry", 1.50, 5, "Cherry Ice Cream");
+            Console.WriteLine("New Item: " + "\nItem Flavor: " + item.Flavor
+                + "\nItem Price: " + item.Price + "\nQuantity of item: "
+                + item.Quantity + "\nItem Description: " + item.Description);
+            Console.ReadKey();
+        }
+    }
+}
